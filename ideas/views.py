6e99b44fa82655d35a5b4763ae9ca73546eda72e
gal <tanjhu@163.com>
2020-05-09 14:34:17 +0800

    將作者包含進模型實例

--- ideas/views.py
+++ ideas/views.py
@@ -22,13 +22,13 @@ def add_or_change_idea(request, pk=None):
         idea = get_object_or_404(Idea, pk=pk)
 
     if request.method == "POST":
-        form = IdeaForm( data=request.POST, files=request.FILES, instance=idea)
+        form = IdeaForm( request,data=request.POST, files=request.FILES, instance=idea)
 
         if form.is_valid():
             idea = form.save()
             return redirect("ideas:idea_detail", pk=idea.pk)
     else:
-        form = IdeaForm( instance=idea)
+        form = IdeaForm(request, instance=idea)
 
     context = {"idea": idea, "form": form}
     return render(request, "ideas/idea_form.html", context)
